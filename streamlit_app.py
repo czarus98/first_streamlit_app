e81160adc78158486d9d3929e8d6d2c9d40dcc1b
Czarek Wojcik <czarek.wojcik98@gmail.com>
2022-10-27 13:25:46 +0200

    Added requests package

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
