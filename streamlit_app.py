2f1bc07a601e14c0dc18147d6af9b0f88618efe1
Czarek Wojcik <czarek.wojcik98@gmail.com>
2022-11-02 15:15:33 +0100

    Added snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,8 @@
 import pandas
-import streamlit
-import snowflake.connector
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input ("What fruit would you like information about?", "Kiwi")
-streamlit.write("The user entered", fruit_choice)
-
 import requests
+import snowflake.connector
+import streamlit
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -25,3 +22,12 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input("What fruit would you like information about?", "Kiwi")
+streamlit.write("The user entered", fruit_choice)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
